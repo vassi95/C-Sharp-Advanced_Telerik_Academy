c83753f2580edba03c2407b14fbe3f0afc3d9223
vassi95 <vassi__95@abv.bg>
2016-05-16 09:34:46 +0300

    Revert "Revert "homework1""
    This reverts commit 0159c4f479586d29eab28c11e45b104ceee7dd93.

---
+++ Arrays/MaximalKSum/MaximalKSum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class MaximalKSum
+{
+    public static void Main()
+    {
+        int length = int.Parse(Console.ReadLine());
+        int maxSumLength = int.Parse(Console.ReadLine());
+
+        int[] arr = new int[length];
+        int maxSum = 0;
+
+        for (int i = 0; i < length; i++)
+        {
+            arr[i] = int.Parse(Console.ReadLine());
+        }
+
+        Array.Sort(arr);
+        Array.Reverse(arr);
+
+        for (int i = 0; i < maxSumLength; i++)
+        {
+            maxSum += arr[i];
+        }
+
+        Console.WriteLine(maxSum);
+    }
+}
