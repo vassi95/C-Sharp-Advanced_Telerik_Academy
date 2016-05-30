5732c6fa116334cfe451bba2cd392cc091f8fc96
vassi95 <vassi__95@abv.bg>
2016-05-30 09:58:31 +0300

    homework6

---
+++ Exception_Handling/EnterNumbers/EnterNumbers/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnterNumbers
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                numbers.Add(int.Parse(Console.ReadLine()));
+            }
+            PrintIfSorted(0, 100, numbers);
+        }
+
+        private static void PrintIfSorted(int start, int end, IList<int> numbers)
+        {
+            try
+            {
+                if (!IsSorted(numbers) || numbers.Any(x => x < start) || numbers.Any(x => x > end))
+                {
+                    throw new ArgumentException();
+                }
+
+                Console.WriteLine("1 < " + string.Join(" < ", numbers) + " < 100");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Exception");
+            }
+        }
+
+        private static bool IsSorted<T>(IList<T> numbers)
+            where T : IComparable
+        {
+            for (int i = 1; i < numbers.Count; i++)
+            {
+                if (numbers[i - 1].CompareTo(numbers[i]) != -1)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Exception_Handling/SquareRoot/SquareRoot/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareRoot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+            TryGetSquareRoot(number);
+        }
+
+        private static void TryGetSquareRoot(string n)
+        {
+            try
+            {
+                double number = double.Parse(n);
+                if (number < 0)
+                {
+                    throw new FormatException("Integer must not be negative");
+                }
+                double square = Math.Sqrt(number);
+                Console.WriteLine("{0:F3}", Math.Round(square, 3));
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid number");
+            }
+            finally
+            {
+                Console.WriteLine("Good bye");
+            }
+        }
+    }
+}
